486b7d36d48cdc93102cc6c379300b5b16b7ea2c
Rozelin <ikeda.m.aj@m.titech.ac.jp>
2020-06-30 22:23:41 +0900

    色々追加

---
+++ src/components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>{{ countMessage }}</div>
+    <button @click="count++">( ﾟ∀ﾟ)ｱﾊﾊ八八ﾉヽﾉヽﾉヽﾉ ＼ / ＼/ ＼</button>
+    <button @click="count = 0">＼(゜ロ＼)(／ロ゜)／</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "counter",
+  data() {
+    return {
+      count: 0
+    };
+  },
+  computed: {
+    countMessage() {
+      return "ｲｴｰｲヽ(^o^)丿: " + this.count;
+    }
+  }
+};
+</script>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div>ItemList</div>
+    <div v-for="item in items" :key="item.name">
+      <div class="item">
+        <div class="name">名前: {{ item.name }}</div>
+        <div class="price">{{ item.price }} 円</div>
+      </div>
+    </div>
+    <div>
+      <label>
+        名前
+        <input
+          type="text"
+          v-model="newItemName"
+          placeholder="商品の名前を入力"
+        />
+      </label>
+      <label>
+        価格
+        <input
+          type="number"
+          v-model="newItemPrice"
+          placeholder="商品の価格を入力"
+        />
+      </label>
+      <button @click="addItem">add</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemList",
+  data() {
+    return {
+      items: [
+        { name: "たまご", price: 100 },
+        { name: "りんご", price: 160 }
+      ],
+      newItemName: "",
+      newItemPrice: ""
+    };
+  },
+  methods: {
+    addItem() {
+      this.items.push({ name: this.newItemName, price: this.newItemPrice });
+      (this.newItemName = ""), (this.newItemPrice = "");
+    }
+  }
+};
+</script>
