f4fc2b9208e81440acdb03c19623368306804082
Rozelin <66714866+Rozelin-dc@users.noreply.github.com>
2021-06-27 21:58:25 +0900

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,35 @@
 module.exports = {
-    "root": true,
-    "env": {
-        "browser": true,
-        "node": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/recommended",
-        "@vue/prettier",
-        "@vue/standard",
-        "@vue/typescript"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "parser": "@typescript-eslint/parser",
-        "ecmaFeatures": {
-            "jsx": false
-        }
-    },
-    "plugins": [
-        "vue",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "prettier/prettier": [
-            "error",
-            {
-              "singleQuote": true,
-              "semi": false,
-              "tabWidth": 2
-            }
-        ],
-        "space-before-function-paren": [2, {"anonymous": "never", "named": "never", "asyncArrow": "always"}],
-        "no-unused-vars": "off"
+  env: {
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    '@vue/prettier',
+    '@vue/standard',
+    '@vue/typescript'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    ecmaFeatures: {
+      jsx: false
     }
-};
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+        tabWidth: 2
+      }
+    ],
+    'space-before-function-paren': [
+      2,
+      { anonymous: 'never', named: 'never', asyncArrow: 'always' }
+    ],
+    'no-unused-vars': 'off'
+  }
+}
